fb2ad5564a80621c99918e96a100b77bd3b80a5d
Grigore Lucian <lucigrigo@yahoo.com>
2020-10-22 23:07:49 +0300

    Create merge_cards.cpp

---
+++ TestSources/C++/KickStart2020G/merge_cards.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int mxN = 1e9;
+
+int T;
+int n[100];
+int v[100][100];
+int ans[100];
+
+void solve(int i) {
+    int D[100][100];
+
+    for(int j = 0; j < n[i] - 1; ++j) {
+        // merging cards
+
+        // choosing maximum merging
+    }
+
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin >> T;
+
+    for(int i = 0; i < T; ++i) {
+        cin >> n[i];
+        for(int j = 0; j < n[i]; ++j) {
+            cin >> v[i][j];
+        }
+    }
+
+    for(int i = 0; i < T; ++i) {
+        solve(i);
+        cout << "Case #" << i + 1 << ": " << ans[i] << endl;
+    }
+
+    return 0;
+}
