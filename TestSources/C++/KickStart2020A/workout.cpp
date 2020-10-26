58752e33a1c775db01684cbc49ccfaf4ea060f9d
Grigore Lucian <lucigrigo@yahoo.com>
2020-10-26 11:13:15 +0200

    Create workout.cpp

---
+++ TestSources/C++/KickStart2020A/workout.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int mxN = 1e6;
+
+void solve(int no_case, int n, int k, int v[mxN])
+{
+    ++no_case;
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    for (int i = 0; i < T; ++i)
+    {
+        int n, k;
+        cin >> n >> k;
+        int v[mxN];
+        for (int j = 0; j < k; ++j)
+        {
+            cin >> v[j];
+        }
+        solve(i, n, k, v);
+    }
+
+    return 0;
+}
