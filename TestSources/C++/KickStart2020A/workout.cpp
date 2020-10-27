c7010cd0a3a42f6cf96d273110ec433790f82efe
Grigore Lucian <luci.grigore99@gmail.com>
2020-10-27 17:25:41 +0200

    Update workout.cpp

--- TestSources/C++/KickStart2020A/workout.cpp
+++ TestSources/C++/KickStart2020A/workout.cpp
@@ -1,12 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define ar array
 const int mxN = 1e6;
 
 void solve(int no_case, int n, int k, int v[mxN])
 {
     ++no_case;
-    
+    int l = 1;
+    int r = v[n - 1] - v[0];
+
+    while(l < r) {
+    	int m = (l + r) / 2;
+    	int k2 = 0;
+    	for(int i = 1; i < n; ++i) {
+    		int d = v[i] - v[i - 1];
+    		k2 += (d + m - 1) / m -1;
+    	}
+    	if(k2 <= k)
+    		r = m;
+    	else
+    		l = m + 1;
+    }
+
+    cout << "Case #" << no_case << ": " << l << endl;
 }
 
 int main()
@@ -18,8 +35,8 @@ int main()
     {
         int n, k;
         cin >> n >> k;
-        int v[mxN];
-        for (int j = 0; j < k; ++j)
+        int v[n];
+        for (int j = 0; j < n; ++j)
         {
             cin >> v[j];
         }
