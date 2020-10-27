1dd34be94f26bd6b6327c087f4b3002e5cbe2f52
Grigore Lucian <luci.grigore99@gmail.com>
2020-10-27 18:06:18 +0200

    Create bundling.cpp

---
+++ TestSources/C++/KickStart2020A/bundling.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ar array
+
+void solve() {
+	
+}
+
+int main() {
+	int T, i = 1;
+	cin >> T;
+	while(T--) {
+		cout << "Case #" << i << " ";
+		solve();
+		++i;
+	}
+}
