d16af24f7a1c6975d0d5d35bd530422047fe6425
Grigore Lucian <luci.grigore99@gmail.com>
2021-05-01 12:53:07 +0300

    Create c.cpp

---
+++ TestSources/CodeJam2021/Round1C/c.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+void solve(int case_no) {
+	
+}
+
+int main() {
+	ll T;
+	cin >> T;
+	while (T--) {
+		solve(T);
+	}
+	return 0;
+}
